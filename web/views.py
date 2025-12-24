703ca910410ff093a014df0217ec1762d782dea0
SumitJainUTD <sumi12june@gmail.com>
2025-12-24 17:33:59 +0530

    Test CICD

--- web/views.py
+++ web/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello Django 👋")
+    return HttpResponse("Hello Django 👋, Testing CICD Pipeline!")
