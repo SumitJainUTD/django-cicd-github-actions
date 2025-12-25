0c6eb101f78135ad20bd19af5d6412e333077bcc
SumitJainUTD <sumi12june@gmail.com>
2025-12-25 19:57:14 +0530

    Ping end point

--- web/urls.py
+++ web/urls.py
@@ -3,8 +3,7 @@ import time
 from django.http import JsonResponse
 from django.urls import path
 
-from web.views import home
-
+from web.views import home, ping
 
 urlpatterns = [
     path('', home),
